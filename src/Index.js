ea77094c982fde0d2425695ef36a311836cdf915
suh79 <siriuh@gmail.com>
2025-11-30 10:39:16 +0100

    Refactor weather functions and add forecast display
    Updated weather display logic and added daily forecast functionality.

--- src/Index.js
+++ src/Index.js
@@ -54,6 +54,13 @@ function searchCity(city) {
   axios.get(apiUrl).then(refreshWeather);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
@@ -70,27 +77,34 @@ function getForecast(city) {
 function displayForecast(response) { 
   console.log(response.data);
 
-  let days = ["Tue", "Wed", "Fri", "Sat", "Sun"];
+  
   let forecastHtml = "";
   
-  days.forEach(function(day){
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5){
     forecastHtml =
       forecastHtml +
        `
        <div class="weather-forecast-day">
-          <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">🌤️</div>
-              <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-date">${formatDay(day.time)}</div>
+
+            <img src="${day.condition.icon_url}" class="weather-forecast-icon">
+            <div class="weather-forecast-temperatures">
                 <div class="weather-forecast-temperature">
-                <strong>15°</strong>
-              </div>
-                <div class="weather-forecast-temperature">9°</div>
-              </div>
-              </div>
-`;
-  });
-  let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = forecastHtml;
+                <strong>${Math.round(day.temperature.maximum)}°</strong>
+                
+                </div>
+                <div class="weather-forecast-temperature">
+                ${Math.round(day.temperature.minimum)}°
+                </div>
+            </div>
+          </div>
+            `; 
+        }
+      });
+      
+      let forecastElement = document.querySelector("#forecast");
+      forecastElement.innerHTML = forecastHtml;
 }
 
 
@@ -102,4 +116,4 @@ let currentDate = new Date();
 
 currentDateELement.innerHTML = formatDate(currentDate);*/
 
-searchCity("Paris");
+searchCity("Grimstad");
