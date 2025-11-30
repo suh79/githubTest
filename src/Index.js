ad5c3fe6a5b2283fcb06375dd201cecc41e1870f
suh79 <siriuh@gmail.com>
2025-11-30 09:17:34 +0100

    Implement forecast fetching and display functionality
    Added getForecast and displayForecast functions to fetch and display weather forecast data.

--- src/Index.js
+++ src/Index.js
@@ -16,6 +16,8 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -59,6 +61,39 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function getForecast(city) {
+  let apiKey = "df32e8cdob8b09f2660834841061ata2";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) { 
+  console.log(response.data);
+
+  let days = ["Tue", "Wed", "Fri", "Sat", "Sun"];
+  let forecastHtml = "";
+  
+  days.forEach(function(day){
+    forecastHtml =
+      forecastHtml +
+       `
+       <div class="weather-forecast-day">
+          <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">🌤️</div>
+              <div class="weather-forecast-temperatures">
+                <div class="weather-forecast-temperature">
+                <strong>15°</strong>
+              </div>
+                <div class="weather-forecast-temperature">9°</div>
+              </div>
+              </div>
+`;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSearchSubmit);
 
@@ -66,4 +101,5 @@ searchForm.addEventListener("submit", handleSearchSubmit);
 let currentDate = new Date();
 
 currentDateELement.innerHTML = formatDate(currentDate);*/
-searchCity("New York");
+
+searchCity("Paris");
